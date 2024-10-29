88d0ce1f7481cb7d6bb37ec4d9ebecc0ba089ece
andrewmigoetz <93052767+andrewmigoetz@users.noreply.github.com>
2024-10-29 01:19:22 -0500

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -3,19 +3,42 @@
 using namespace std;
 
 class Temperature {
-	private:
-		double degreesKelvin,
-		       degreesFahrenheit,
-		       degreesCelsius;
-	public:
-		void setTempKelvin(double degreesKelvin) {  }
-		double getTempKelvin() {  }
-		void setTempCelsius() {  }
-		double getTempCelsius() {  } 
-		void setTempFahrenheit() {  }
-		double getTempFahrenheit() { }
+private:
+    double degreesKelvin,
+           degreesFahrenheit,
+           degreesCelsius;
+public:
+    // For each public function, return or set a value based on private variables:
+    void setTempKelvin(double kelvin) { degreesKelvin = kelvin; }
+    double getTempKelvin() { return degreesKelvin; }
+    // With kelvin formula, solve for celsius
+    void setTempCelsius(double kelvin) { degreesCelsius = kelvin-273.15; }
+    double getTempCelsius() { return degreesCelsius; }
+    // Isolate fahrenheit and model relationship between kelvin and fahrenheit
+    void setTempFahrenheit(double kelvin) { degreesFahrenheit = ((kelvin-273.15) * 9/5) + 32; }
+    double getTempFahrenheit() { return degreesFahrenheit; }
 };
 
 int main() {
+    // Define variables
+    double kelvin;
+
+    // Define class object
+    Temperature temperature;
+
+    // Prompt user to enter temperature in degrees Kelvin
+    cout << "Enter a temperature in degrees Kelvin: " << endl;
+    cin >> kelvin;
+
+    // Set temperatures of celsius and fahrenheit based upon kelvin input
+    temperature.setTempKelvin(kelvin);
+    temperature.setTempCelsius(kelvin);
+    temperature.setTempFahrenheit(kelvin);
+
+    // Return temperature in kelvin, celsius, and fahrenheit
+    cout << "Celsius: " << temperature.getTempCelsius() << endl;
+    cout << "Fahrenheit: " << temperature.getTempFahrenheit() << endl;
+
+
 
 }
